92834e29d37e24fdf316499746cedfc2ec287e6e
Saikat Maji <saikatmaji2006@gmail.com>
2025-02-14 01:20:44 +0530

    encryption using bcrypt and jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema({
     UserName : {
         type : String,
@@ -49,4 +51,45 @@ const userSchema = new mongoose.Schema({
 {
     timestamps : true,
 })
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")){
+    this.password = bcrypt.hash(this.password,11)
+    next()
+    }
+})
+userSchema.methods.isPasswordCorrect = async function 
+(password) {
+   return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.generatAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            Email : this.Email,
+            UserName : this.UserName,
+            FullName : this.FullName,
+
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+
+    )
+}
+userSchema.methods.generatRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+           
+
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+
+    )
+}
 export const user = mongoose.model("user",userSchema)
